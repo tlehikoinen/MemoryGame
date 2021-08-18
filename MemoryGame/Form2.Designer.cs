8dab91d84800f7c63aac360f40560678c913a11f
Tommi <akuankka75@gmail.com>
2021-08-18 19:29:42 +0300

    randomises pictures to grid

--- MemoryGame/Form2.Designer.cs
+++ MemoryGame/Form2.Designer.cs
@@ -27,6 +27,8 @@ namespace MemoryGame {
             this.button3 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.memoryGameGrid = new System.Windows.Forms.TableLayoutPanel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button3
@@ -62,13 +64,34 @@ namespace MemoryGame {
             this.memoryGameGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.memoryGameGrid.Size = new System.Drawing.Size(600, 600);
             this.memoryGameGrid.TabIndex = 8;
-            this.memoryGameGrid.Paint += new System.Windows.Forms.PaintEventHandler(this.memoryGameGrid_Paint);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(673, 177);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(673, 218);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(769, 624);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.memoryGameGrid);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button3);
@@ -90,5 +113,7 @@ namespace MemoryGame {
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.TableLayoutPanel memoryGameGrid;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
--- MemoryGame/Form2.cs
+++ MemoryGame/Form2.cs
@@ -6,40 +6,96 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using System.Linq;
 
 namespace MemoryGame {
     public partial class Form2 : Form {
         Memory game = new Memory();
+        GridSizeInNumber grid = new GridSizeInNumber();
+        List<string> pictureAddresses = new List<string>();
+        int[] shuffledNumbers;
+        Panel clicked1Panel = new Panel();
 
         public Form2(Memory game) {
             this.game = game;
             InitializeComponent();
+            randomiseAddresses();
+        }
+
+
+        // This function returns integer array presentation of shuffled cards
+        private void randomiseAddresses() {
+            Random rnd = new Random();
+
+            // Big grid 6x6 will have 18 different cards
+            int[] maxCards = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+            maxCards = maxCards.OrderBy(x => rnd.Next()).ToArray();
+            // Get card count according to game state
+            var gridSize = gridSizeInNumber(game.getGridSize());
+            int neededCards = gridSize.column * gridSize.row / 2;
+
+            // Each cards needs duplicate
+            int[] newCards = new int[neededCards*2];
+            int j = 0;
+
+            // Save duplicates!
+            for (int i = 0; i < newCards.Length; i++) {
+                newCards[i] = maxCards[j];
+                j++;
+                if (j == neededCards)
+                    j = 0;
+              }
+
+            // Randomise again
+            newCards = newCards.OrderBy(x => rnd.Next()).ToArray();
+            this.shuffledNumbers = newCards;
         }
 
         private void Form2_Load(object sender, EventArgs e) {
             initialiseGrid();
+            getImages(pictureFolderPath(game.getDeckSelection()));
         }
 
-        private int gridSizeInNumber(Memory.GridSize size) {
+        private GridSizeInNumber gridSizeInNumber(Memory.GridSize size) {
             switch ((Memory.GridSize)Enum.Parse(typeof(Memory.GridSize), size.ToString())) {
                 case Memory.GridSize.small: {
-                    return 4;
+                    grid.column = 4;
+                    grid.row = 3;
+                    return grid;
                 }
                 case Memory.GridSize.medium: {
-                    return 6;
+                    grid.column = 5;
+                    grid.row = 4;
+                    return grid;
                 }
                 case Memory.GridSize.big: {
-                    return 8;
+                    grid.column = 6;
+                    grid.row = 6;
+                    return grid;
                 }
                 default:
-                    return 0;
+                  return grid;
             }
         }
 
+        private string pictureFolderPath(Memory.DeckSelection deck) {
+            switch ((Memory.DeckSelection)Enum.Parse(typeof(Memory.DeckSelection), deck.ToString())) {
+                case Memory.DeckSelection.cats: {
+                    return "../../../Pictures/Images/Cats";
+                }
+                case Memory.DeckSelection.characters: {
+                    return "../../../Pictures/Images/Characters";
+                }
+                default:
+                return "";
+            }
+
+        }
+
         private void initialiseGrid() {
-            memoryGameGrid.ColumnCount = gridSizeInNumber(game.getGridSize());
-            memoryGameGrid.RowCount = gridSizeInNumber(game.getGridSize());
-            var grids = gridSizeInNumber(game.getGridSize());
+            memoryGameGrid.ColumnCount = gridSizeInNumber(game.getGridSize()).column;
+            memoryGameGrid.RowCount = gridSizeInNumber(game.getGridSize()).row;
+            var grids = memoryGameGrid.ColumnCount;
             var padding = 5;
             var size = this.memoryGameGrid.Size.Width;
 
@@ -57,25 +113,35 @@ namespace MemoryGame {
                 style.SizeType = SizeType.Absolute;
                 style.Height = boxSize;
             }
-
+            int k = 0;
             for (int i= 0; i < memoryGameGrid.ColumnCount; i++) {
                 for(int j = 0; j < memoryGameGrid.RowCount; j++) {
                     Panel panel = new Panel {
                         Size = new Size(pictureSize, pictureSize),
                     };
+                    int sum = k;
                     string rowPos = i + "," + j;
                     panel.Click += memoryGamePanelClick;
-                    panel.Name = rowPos;
+                    panel.Name = sum.ToString();
                     panel.BorderStyle = BorderStyle.Fixed3D;
                     memoryGameGrid.Controls.Add(panel, i, j);
+                    k++;
                 }
             }
         }
 
         void memoryGamePanelClick(object sender, EventArgs e) {
+            Random rnd = new Random();
             Panel panel = sender as Panel;
-            if(panel.BackgroundImage == null) { 
-            panel.BackgroundImage = resizeImage(Image.FromFile("../../../Pictures/Game/1.png"), panel.Size);
+            if(panel.BackgroundImage == null) {
+                try {
+                    clicked1Panel.BackgroundImage = null;
+                    panel.BackgroundImage = resizeImage(Image.FromFile(pictureAddresses[shuffledNumbers[int.Parse(panel.Name)]]), panel.Size);
+                    clicked1Panel = panel;
+                } catch {
+                    panel.BackgroundImage = null;
+                }
+            
             } else {
                 panel.BackgroundImage = null;
             }
@@ -96,8 +162,27 @@ namespace MemoryGame {
             game.updateData();
         }
 
-        private void memoryGameGrid_Paint(object sender, PaintEventArgs e) {
-
+        private void button7_Click(object sender, EventArgs e) {
+            string combinedPath = pictureFolderPath(game.getDeckSelection());
+            getImages(combinedPath);
         }
+
+        private void getImages(string path) {
+            var files = Directory.GetFiles(path);
+            foreach (string file in files) {
+                pictureAddresses.Add(file);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e) {
+            foreach (int item in shuffledNumbers) {
+                System.Diagnostics.Debug.WriteLine(item);
+            }
+        }
+    }
+
+    public class GridSizeInNumber {
+        public int column;
+        public int row;
     }
 }
